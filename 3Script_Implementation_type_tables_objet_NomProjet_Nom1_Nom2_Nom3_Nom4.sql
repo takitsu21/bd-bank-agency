550ef008c901b69f0f2c28d4880aa41e93665939
takitsu21 <dylannbatisse@gmail.com>
2021-12-20 09:24:44 +0100

    add ressources

---
+++ 3Script_Implementation_type_tables_objet_NomProjet_Nom1_Nom2_Nom3_Nom4.sql
@@ -0,0 +1,40 @@
+/*
+Nom, Prénom Participant 1:			, Activités paricipant1
+Nom, Prénom Participant 2:			, Activités paricipant2
+Nom, Prénom Participant 3:			, Activités paricipant3
+
+*/
+
+/*
+
+2.	Implémentation des types et tables objets
+Le résultat de cette phase doit être mis dans un fichier appelé 
+3Script_Implementation_type_tables_objet_NomProjet_Nom1_Nom2_Nom3_Nom4.sql
+2.1	Création des types à partir du schéma de types
+Proposer la création des types (partie spécification) avec l’ensembles des champs et des méthodes y compris les champs pour gérer les liens d’association.
+2.2	Création des tables objets et des indexes à partir des types créés auparavant
+Définir le schéma physique consiste à produire les ordres SQL de création des tables objets, indexes etc.. 
+Si vous avez une base de données Oracle locale, il faut créer un utilisateur Oracle si ce n’est déjà fait ou utilser le compte Oracle qui vous a été fourni sur une base distante. Cet utilisateur sera le propriétaire de tous les objets de votre application (types, des tables objets, indexes, ...).
+
+Vous devez aussi poser les indexes sur vos colonnes REF y compris dans les listes.
+2.3	Insertion des lignes dans vos tables objets
+Il s’agit d’effectuer manuellement des insertions de lignes dans chacunes de vos tables. Insérer 10 à 20 lignes par tables. Bien gérer les contraintes d’intégrités (primary key, check, non nul).
+2.4	Mise à jour et consultation des données dans vos tables objets
+Les requêtes de mise à jour (modification, suppression) et de consulatation à écrire sont celles définies dans le chapitre 1.
+2.5	Implémentation des méthodes de vos types en PLSQL 
+Il s’agit définir les types Body et d’implémenter le code des méthodes des types définis dans la spécification des types. 
+
+Vous devez aussi proposer le code de test de chacune des méthodes.
+
+2.6	Travail à rendre (04/01/2022)
+Le travail à rendre doit être dans le fichier :
+Script_Implementation_type_tables_objet_NomProjet_Nom1_Nom2_Nom3_Nom4.sql
+Vous devez y mettre :
+•	Création des types à partir du schéma de types
+•	Création des tables objets et des indexes à partir des types créés auparavant
+•	Insertion des lignes dans vos tables objets
+•	Mise à jour et consultation des données dans vos tables objets
+•	Implémentation des méthodes de vos types en PLSQL
+
+
+*/
